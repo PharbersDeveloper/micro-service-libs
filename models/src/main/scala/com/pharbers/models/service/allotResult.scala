5b9815db0a082a7f532c5e0bf1a5b94e201de4b8
clock <2567729201@qq.com>
2018-09-14 17:45:44 +0800

    被杨总抓到了

---
+++ models/src/main/scala/com/pharbers/models/service/allotResult.scala
@@ -0,0 +1,7 @@
+package com.pharbers.models.service
+
+import com.pharbers.macros.api.commonEntity
+
+class allotResult extends commonEntity {
+    var report_id: String = ""
+}
---
+++ models/src/main/scala/com/pharbers/models/service/managerinputinfo.scala
@@ -0,0 +1,7 @@
+package com.pharbers.models.service
+
+import com.pharbers.macros.api.commonEntity
+
+class managerinputinfo extends commonEntity {
+    var total_day: Int = 0
+}
