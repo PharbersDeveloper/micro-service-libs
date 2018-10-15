eb1bde168d923f7ab3cea84d8bc6b98e2e58c8ca
dcs <419787760@qq.com>
2018-10-15 07:53:18 -0400

    压测gethome

--- gatling/src/test/scala/com/pharbers/gatling/simulation/userLogin.scala
+++ gatling/src/test/scala/com/pharbers/gatling/simulation/userLogin.scala
@@ -9,16 +9,17 @@ import com.pharbers.gatling.base.phHttpProtocol
 class userLogin extends Simulation {
 	import com.pharbers.gatling.base.phHttpProtocol.{noneBlackList, noneWhiteList}
 
-	val httpProtocol = phHttpProtocol("http://123.56.179.133:18002")
+	val httpProtocol = phHttpProtocol("http://123.56.179.133:7434")
 
 	val scn = scenario("user_login")
 		.exec(
 			getHome.getHome
 					.pause(5 seconds)
-//			,userLogin.login
-//					.pause(60 seconds)
+			,
+			userLogin.login
+					.pause(5 seconds)
 		)
 
-	setUp(scn.inject(rampUsers(1000) over (3 seconds))).protocols(httpProtocol)
+	setUp(scn.inject(rampUsers(100) over (10 seconds))).protocols(httpProtocol)
 
 }
