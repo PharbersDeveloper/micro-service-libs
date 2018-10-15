cb9bfaba258e5f8b3dad9badd0ccb6d47ba8c662
clock <2567729201@qq.com>
2018-10-15 19:50:40 +0800

    gatling 引入宏

--- gatling/src/test/scala/com/pharbers/gatling/scenario/userLogin.scala
+++ gatling/src/test/scala/com/pharbers/gatling/scenario/userLogin.scala
@@ -3,15 +3,31 @@ package com.pharbers.gatling.scenario
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.core.structure.ChainBuilder
-
 import com.pharbers.gatling.base.phHeaders.headers_json
+import com.pharbers.jsonapi.json.circe.CirceJsonapiSupport
+import com.pharbers.models.request.request
 
 object userLogin {
 	val feeder = csv("loginUser.csv").random
 	println(feeder)
 
-	val login: ChainBuilder = exec(http("login")
-			.get("/api/user/login")
+	import io.circe.syntax._
+	import com.pharbers.macros._
+	import com.pharbers.models.request._
+	import com.pharbers.macros.convert.jsonapi.JsonapiMacro._
+	val a = new CirceJsonapiSupport{}
+	import a._
+
+	val rq = new request
+	rq.res = "user"
+	val eq1 = eq2c("email", "zyqi@pharbers.com")
+	val eq2 = eq2c("password", "zyqi@pharbers.com")
+	rq.eqcond = Some(eq1 :: eq2 :: Nil)
+
+	val login: ChainBuilder = exec(
+		http("login")
+			.post("/api/v1/login/0")
 			.headers(headers_json)
-			.body(StringBody("""{ "condition" :  { "email" : "nhwa", "password" : "nhwa" } }""")).asJSON)
+			.body(StringBody(toJsonapi(rq).asJson.noSpaces)).asJSON
+	)
 }
--- gatling/src/test/scala/com/pharbers/gatling/simulation/courseLst.scala
+++ gatling/src/test/scala/com/pharbers/gatling/simulation/courseLst.scala
@@ -1,24 +1,22 @@
 package com.pharbers.gatling.simulation
 
 import io.gatling.core.Predef._
-
+import scala.language.postfixOps
 import scala.concurrent.duration._
 import com.pharbers.gatling.scenario._
 import com.pharbers.gatling.base.phHttpProtocol
 import io.gatling.core.structure.ScenarioBuilder
 
-import scala.language.postfixOps
-
-class userLogin extends Simulation {
+class courseLst extends Simulation {
 	import com.pharbers.gatling.base.phHttpProtocol.{noneBlackList, noneWhiteList}
 
-	val httpProtocol = phHttpProtocol("http://127.0.0.1:9000")
+	val httpProtocol = phHttpProtocol("http://123.56.179.133:18002")
 
-	val scn: ScenarioBuilder = scenario("login")
+	val scn: ScenarioBuilder = scenario("courseLst")
 		.exec(
-			userLogin.login.pause(5 seconds)
+			courseLst.courseLst.pause(5 seconds)
 		)
 
-	setUp(scn.inject(rampUsers(1) over (3 seconds))).protocols(httpProtocol)
+	setUp(scn.inject(rampUsers(10) over (3 seconds))).protocols(httpProtocol)
 
 }
