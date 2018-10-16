95a2443580a1d744584da6cfffb36e84382da16c
clock <2567729201@qq.com>
2018-10-16 22:05:45 +0800

    节点全部拆分

---
+++ models/src/main/scala/com/pharbers/models/service/callapmr.scala
@@ -0,0 +1,9 @@
+package com.pharbers.models.service
+
+import com.pharbers.macros.api.commonEntity
+import com.pharbers.macros.common.connecting.ToStringMacro
+
+@ToStringMacro
+class callapmr extends commonEntity {
+    var paper_id: String = ""
+}
