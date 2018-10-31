82b01b706919435ebd45c599c8647dcba0d0158c
dcs <419787760@qq.com>
2018-10-30 22:35:55 -0400

    r

--- macros/src/main/scala/com/pharbers/macros/api/JsonapiConvert.scala
+++ macros/src/main/scala/com/pharbers/macros/api/JsonapiConvert.scala
@@ -4,9 +4,6 @@ import com.pharbers.jsonapi.model.RootObject
 
 trait JsonapiConvert[T] {
     def fromJsonapi(jsonapi: RootObject): T
-
-    def fromJsonapiLst(jsonapi: RootObject): List[T]
-
     def toJsonapi(obj: T): RootObject
     def toJsonapi(objs: List[T]): RootObject
 }
