354b46ac65cd626008bc5108761df5459a8bc7d3
dcs <419787760@qq.com>
2018-10-31 07:23:47 -0400

    添加解析含有incond字段请求，及将key为id的value转换为objectid

--- models/src/main/scala/com/pharbers/models/request/incond.scala
+++ models/src/main/scala/com/pharbers/models/request/incond.scala
@@ -13,14 +13,6 @@ case class incond() extends commonEntity with TraitConditions {
     var `val`: Iterable[Any] = null
     var category: Any = null
 
-    //old
-
-//    override def cond2QueryDBObject(): DBObject = key match {
-//        case "id" => DBObject("_id" -> DBObject("$in" -> `val`))
-//        case "_id" => DBObject("_id" -> DBObject("$in" -> `val`))
-//        case _ => DBObject(key ->  DBObject("$in" -> `val`))
-//    }
-
     override def cond2QueryDBObject(): DBObject = key match {
         case "id" =>
             DBObject("_id" -> DBObject("$in" -> `val`.map(x => new ObjectId(x.toString)).toSet))
@@ -28,7 +20,6 @@ case class incond() extends commonEntity with TraitConditions {
             DBObject("_id" -> DBObject("$in" -> `val`.map(x => new ObjectId(x.toString)).toSet))
         case _ => DBObject(key ->  DBObject("$in" -> `val`))
     }
-
 
     override def cond2UpdateDBObectj(): DBObject = DBObject()
 
