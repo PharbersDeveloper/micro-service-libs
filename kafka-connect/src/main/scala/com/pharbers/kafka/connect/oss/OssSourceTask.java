84744c16b5e1b25f34d1d5737251ecb76b9fde4e
ZhangChi <jeorch@foxmail.com>
2019-07-02 16:32:13 +0800

    pharbers-kafka-connect oss source succeed!
    Test Passed

--- kafka-connect/src/main/scala/com/pharbers/kafka/connect/oss/OssSourceTask.java
+++ kafka-connect/src/main/scala/com/pharbers/kafka/connect/oss/OssSourceTask.java
@@ -6,7 +6,6 @@ import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.OSSObject;
 import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
@@ -17,9 +16,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -46,8 +42,6 @@ public class OssSourceTask extends SourceTask {
     private String ossKey;
     private InputStream stream;
     private BufferedReader reader = null;
-    private char[] buffer = new char[1024];
-    private int offset = 0;
     private String topic = null;
     private int batchSize = OssSourceConnector.DEFAULT_TASK_BATCH_SIZE;
 
@@ -69,7 +63,6 @@ public class OssSourceTask extends SourceTask {
             stream = System.in;
             // Tracking offset for stdin doesn't make sense
             streamOffset = null;
-            reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
         }
         // Missing topic or parsing error is not possible because we've parsed the config in the
         // Connector
@@ -78,7 +71,7 @@ public class OssSourceTask extends SourceTask {
     }
 
     @Override
-    public List<SourceRecord> poll() throws InterruptedException {
+    public List<SourceRecord> poll() {
 
         OSS client = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
 
@@ -88,32 +81,34 @@ public class OssSourceTask extends SourceTask {
                 OSSObject object = client.getObject(bucketName, ossKey);
                 log.info("Contest-Type: " + object.getObjectMetadata().getContentType());
                 stream = object.getObjectContent();
-
-                Map<String, Object> offset = context.offsetStorageReader().offset(Collections.singletonMap(FILENAME_FIELD, ossKey));
-                if (offset != null) {
-                    Object lastRecordedOffset = offset.get(POSITION_FIELD);
-                    if (lastRecordedOffset != null && !(lastRecordedOffset instanceof Long))
-                        throw new ConnectException("Offset position is the incorrect type");
-                    if (lastRecordedOffset != null) {
-                        log.debug("Found previous offset, trying to skip to file offset {}", lastRecordedOffset);
-                        long skipLeft = (Long) lastRecordedOffset;
-                        while (skipLeft > 0) {
-                            try {
-                                long skipped = stream.skip(skipLeft);
-                                skipLeft -= skipped;
-                            } catch (IOException e) {
-                                log.error("Error while trying to seek to previous offset in file {}: ", ossKey, e);
-                                throw new ConnectException(e);
-                            }
-                        }
-                        log.debug("Skipped to offset {}", lastRecordedOffset);
-                    }
-                    streamOffset = (lastRecordedOffset != null) ? (Long) lastRecordedOffset : 0L;
-                } else {
-                    streamOffset = 0L;
-                }
+                streamOffset = 0L;
                 reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
-                log.debug("Opened {} for reading", logFilename());
+                ArrayList<SourceRecord> records = null;
+
+                String line;
+                do {
+                    line = reader.readLine();
+                    if (line != null) {
+                        log.trace("Read a line from {}", logFilename());
+                        if (records == null)
+                            records = new ArrayList<>();
+                        streamOffset += (line.length() + 1);
+                        records.add(new SourceRecord(offsetKey(ossKey), offsetValue(streamOffset), topic, null,
+                                null, null, VALUE_SCHEMA, line, System.currentTimeMillis()));
+
+                        if (records.size() >= batchSize) {
+                            return records;
+                        }
+                    }
+                } while (line != null);
+
+                reader.close();
+                log.info("records over");
+                return records;
+
+            } catch (IOException e) {
+                // Underlying stream was killed, probably as a result of calling stop. Allow to return
+                // null, and driving thread will handle any shutdown if necessary.
             } catch (OSSException oe) {
                 System.out.println("Caught an OSSException, which means your request made it to OSS, "
                         + "but was rejected with an error response for some reason.");
@@ -133,103 +128,16 @@ public class OssSourceTask extends SourceTask {
                 client.shutdown();
             }
         }
-
-        // Unfortunately we can't just use readLine() because it blocks in an uninterruptible way.
-        // Instead we have to manage splitting lines ourselves, using simple backoff when no new data
-        // is available.
-        try {
-            final BufferedReader readerCopy;
-            synchronized (this) {
-                readerCopy = reader;
-            }
-            if (readerCopy == null)
-                return null;
-
-            ArrayList<SourceRecord> records = null;
-
-            int nread = 0;
-            while (readerCopy.ready()) {
-                nread = readerCopy.read(buffer, offset, buffer.length - offset);
-                log.trace("Read {} bytes from {}", nread, logFilename());
-
-                if (nread > 0) {
-                    offset += nread;
-                    if (offset == buffer.length) {
-                        char[] newbuf = new char[buffer.length * 2];
-                        System.arraycopy(buffer, 0, newbuf, 0, buffer.length);
-                        buffer = newbuf;
-                    }
-
-                    String line;
-                    do {
-                        line = extractLine();
-                        if (line != null) {
-                            log.trace("Read a line from {}", logFilename());
-                            if (records == null)
-                                records = new ArrayList<>();
-                            records.add(new SourceRecord(offsetKey(ossKey), offsetValue(streamOffset), topic, null,
-                                    null, null, VALUE_SCHEMA, line, System.currentTimeMillis()));
-
-                            if (records.size() >= batchSize) {
-                                return records;
-                            }
-                        }
-                    } while (line != null);
-                }
-            }
-
-            if (nread <= 0)
-                synchronized (this) {
-                    this.wait(1000);
-                }
-
-            return records;
-        } catch (IOException e) {
-            // Underlying stream was killed, probably as a result of calling stop. Allow to return
-            // null, and driving thread will handle any shutdown if necessary.
-        }
         return null;
-    }
-
-    private String extractLine() {
-        int until = -1, newStart = -1;
-        for (int i = 0; i < offset; i++) {
-            if (buffer[i] == '\n') {
-                until = i;
-                newStart = i + 1;
-                break;
-            } else if (buffer[i] == '\r') {
-                // We need to check for \r\n, so we must skip this if we can't check the next char
-                if (i + 1 >= offset)
-                    return null;
-
-                until = i;
-                newStart = (buffer[i + 1] == '\n') ? i + 2 : i + 1;
-                break;
-            }
-        }
-
-        if (until != -1) {
-            String result = new String(buffer, 0, until);
-            System.arraycopy(buffer, newStart, buffer, 0, buffer.length - newStart);
-            offset = offset - newStart;
-            if (streamOffset != null)
-                streamOffset += newStart;
-            return result;
-        } else {
-            return null;
-        }
     }
 
     @Override
     public void stop() {
-        log.trace("Stopping");
+        log.info("Stopping");
         synchronized (this) {
             try {
-                if (stream != null && stream != System.in) {
-                    stream.close();
-                    log.trace("Closed input stream");
-                }
+                stream.close();
+                log.info("Closed input stream");
             } catch (IOException e) {
                 log.error("Failed to close FileStreamSourceTask stream: ", e);
             }
