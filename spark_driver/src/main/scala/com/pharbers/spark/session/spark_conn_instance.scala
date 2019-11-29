fe56aa8db78583e66b51532f3d88290a576a9ee2
clock <2567729201@qq.com>
2019-11-29 21:38:47 +0800

    修改 spark yarn 端口

--- spark_driver/src/main/scala/com/pharbers/spark/session/spark_conn_instance.scala
+++ spark_driver/src/main/scala/com/pharbers/spark/session/spark_conn_instance.scala
@@ -22,15 +22,15 @@ trait spark_conn_instance {
     val path = "hdfs:///jars/maxJars/"
     val jarslst: Array[String] = jars.map(jar => path + jar)
     private val conf = new SparkConf()
-            .set("spark.yarn.jars", "hdfs://spark.master:9000/jars/sparkJars")
-            .set("spark.yarn.archive", "hdfs://spark.master:9000/jars/sparkJars")
+            .set("spark.yarn.jars", "hdfs://spark.master:8020/jars/sparkJars")
+            .set("spark.yarn.archive", "hdfs://spark.master:8020/jars/sparkJars")
             .set("yarn.resourcemanager.hostname", "spark.master")
             .set("yarn.resourcemanager.address", "spark.master:8032")
             .setAppName(applicationName)
             .setMaster("yarn")
             .set("spark.scheduler.mode", "FAIR")
             .set("spark.sql.crossJoin.enabled", "true")
-            .set("spark.yarn.dist.files", "hdfs://spark.master:9000/config")
+            .set("spark.yarn.dist.files", "hdfs://spark.master:8020/config")
             .set("spark.executor.memory", "2g")
             .set("spark.driver.extraJavaOptions", "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,adress=5005")
             .set("spark.executor.extraJavaOptions",
